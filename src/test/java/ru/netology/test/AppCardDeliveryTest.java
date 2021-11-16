01d1f82254ab43b30169bd9563e7d27b0912f302
imperfectSD <imperfect.sparda@gmail.com>
2021-11-16 17:18:57 +0300

    allure modified

--- src/test/java/ru/netology/test/AppCardDeliveryTest.java
+++ src/test/java/ru/netology/test/AppCardDeliveryTest.java
@@ -1,7 +1,10 @@
 package ru.netology.test;
 
-import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 import ru.netology.data.DataGenerator;
@@ -21,6 +24,16 @@ public class AppCardDeliveryTest {
         @BeforeEach
         void setup() {
                 open("http://localhost:9999");
+        }
+
+        @BeforeAll
+        static void setUpAll() {
+                SelenideLogger.addListener("allure", new AllureSelenide());
+        }
+
+        @AfterAll
+        static void tearDownAll() {
+                SelenideLogger.removeListener("allure");
         }
 
         @Test
